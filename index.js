739549e3de70a3e0d140952f0d5e3a3b21f6a7ec
Steve Peters <sixhops@gmail.com>
2017-06-25 17:38:41 -0700

    Get params from request url.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const sqlite = require('sqlite'),
       express = require('express'),
       app = express();
 
+const url = require('url');
+
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
 
 // START SERVER
@@ -16,7 +18,14 @@ app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  res.status(500).send('Not Implemented');
+  // Retrieve the params from the url
+  var parentId = req.params.id;
+  var parsedQuery = url.parse(req.url, true).query;
+  var offset = parsedQuery.offset;
+  var limit = parsedQuery.limit;
+
+  
+  //res.status(500).send('Not Implemented');
 }
 
 module.exports = app;
